a22216f4d72e4b7543c4a5b682274ec55bd5880f
Collin Hicks <hicksc14@proton.me>
2025-05-26 14:33:46 -0400

    Update main.rs
    removing comments

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,12 @@ mod task;
 use ampq::connection::setup_messaging;
 
 
-
-
-
-
-
-
-
-
-// // sample task macro usage
-// #[task(name = "example_task")]
-// async fn example_task(payload: Value, task: Task) -> Result<(), Box<dyn std::error::Error>> {
-//     println!("Running task: {}", task.id);
-//     println!("Payload: {:?}", payload);
-//     Ok(())
-// }
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 env_logger::init();
 let queue_names: Vec<&'static str> = vec!["test_queue_1", "test_queue_2"];
 setup_messaging(&queue_names).await.expect("Failed to setup messaging");
 println!("Messaging setup complete");
-// // sample task macro usage
 
 tokio::signal::ctrl_c().await?;
 println!("Shutdown signal received.");
